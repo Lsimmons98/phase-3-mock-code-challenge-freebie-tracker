be05dc7257a5e66cc02a459b1c7bde3f5a236da1
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-22 13:44:48 -0400

    Create freebies migration and relevant class methods

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    self.freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(dev, freebie)
+    return unless freebie.dev == self
+
+    freebie.update(dev: dev)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev_rick = Dev.create(name: "Rick")
+dev_morty = Dev.create(name: "Morty")
+dev_meseeks = Dev.create(name: "Mr. Meseeks")
+dev_gazorp = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Pen", value: 5, dev: dev_rick, company: google)
+Freebie.create(item_name: "Paper Clip", value: 3, dev: dev_morty, company: facebook)
+Freebie.create(item_name: "Eraser", value: 25, dev: dev_rick, company: dunder)
+Freebie.create(item_name: "Cup", value: 16, dev: dev_rick, company: enron)
 
 puts "Seeding done!"
